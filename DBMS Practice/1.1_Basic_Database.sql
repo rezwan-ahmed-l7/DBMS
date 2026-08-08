a8c03ef9dcb711e45c274a37e8904d11e07452db
Rezwan <rezwanahmedtorabee@gmail.com>
2026-08-09 01:06:25 +0600

    DBMS

--- DBMS Practice/1.1_Basic_Database.sql
+++ DBMS Practice/1.1_Basic_Database.sql
@@ -10,5 +10,8 @@ CREATE TABLE Student (
 
 INSERT INTO Student VALUES (1, 'Paris', 24);
 INSERT INTO Student VALUES (2, 'Rezwan', 25);
+INSERT INTO Student VALUES (3, 'Nayeem', 22);
+INSERT INTO Student VALUES (4, 'Rafi', 23);
+INSERT INTO Student VALUES (5, 'Imran', 26);
 
 SELECT * FROM Student;
