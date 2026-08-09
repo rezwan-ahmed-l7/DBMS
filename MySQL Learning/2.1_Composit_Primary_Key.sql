d6004ce1f46a5076dcccd5368d5b28aef51c752e
Rezwan <rezwanahmedtorabee@gmail.com>
2026-08-10 01:35:24 +0600

    SQL

---
+++ MySQL Learning/2.1_Composit_Primary_Key.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS Google;
+CREATE DATABASE Google;
+USE Google;
+
+CREATE TABLE Employee (
+    ID INT,
+    Name VARCHAR(50),
+    Salary INT,
+    PRIMARY KEY (ID, Name)          -- Composite Primary Key
+);
+
+INSERT INTO Employee (ID, Name, Salary)
+VALUES
+(1, "Paris", 10000),
+(2, "Nafis", 8000),
+(3, "Ryhan", 9000),
+(4, "Shakib", 12000),
+(5, "Tamim", 7500),
+(6, "Mushfiq", 11000);
+
+SELECT * FROM Employee;
