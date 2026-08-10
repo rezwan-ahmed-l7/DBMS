c248ff7993c7aadaaa62d1d450f7b620f9c9c66f
Rezwan <rezwanahmedtorabee@gmail.com>
2026-08-11 03:11:06 +0600

    SQL

---
+++ MySQL Learning/2.4_Practice.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS Room;
+CREATE DATABASE Room;
+USE Room;
+
+CREATE TABLE People(
+	ID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Marks INT NOT NULL,
+    Grade VARCHAR(1),
+    City VARCHAR(50)
+);
+
+INSERT INTO People
+(ID, Name, Marks, Grade, City)
+
+value
+(101, 'Alif', 30, 'F', 'Rangpur'),
+(102, 'Nice', 45, 'D', 'Saidpur'),
+(103, 'Sahin', 55, 'C', 'Dhaka'),
+(104, 'Ripon', 68, 'B', 'Dinajpur'),
+(105, 'Prince', 89, 'A', 'Dhaka'),
+(106, 'Nayeem', 72, 'A', 'Chittagong'),
+(107, 'Rafi', 39, 'F', 'Rangpur'),
+(108, 'Imran', 81, 'A', 'Dhaka'),
+(109, 'Tanvir', 60, 'B', 'Saidpur'),
+(110, 'Fahim', 47, 'D', 'Chittagong');
+
+SELECT * FROM People;
