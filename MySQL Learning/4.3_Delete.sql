78d0f463763bf9168c870fe87e10d82a5d0c3723
Rezwan <rezwanahmedtorabee@gmail.com>
2026-08-16 00:42:25 +0600

    SQL

---
+++ MySQL Learning/4.3_Delete.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS Room;
+CREATE DATABASE Room;
+USE Room;
+
+CREATE TABLE People(
+	ID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Marks INT NOT NULL,
+    Grade VARCHAR(1),
+    City VARCHAR(50)
+);
+
+INSERT INTO People
+(ID, Name, Marks, Grade, City)
+
+value
+(101, 'Alif', 30, 'F', 'Rangpur'),
+(102, 'Nice', 45, 'D', 'Dinajpur'),
+(103, 'Sahin', 85, 'C', 'Dhaka'),
+(104, 'Ripon', 68, 'B', 'Dinajpur'),
+(105, 'Prince', 89, 'A', 'Dhaka'),
+(106, 'Nayeem', 82, 'A', 'Chittagong'),
+(107, 'Rafi', 39, 'F', 'Khulna'),
+(108, 'Imran', 81, 'A', 'Rangpur'),
+(109, 'Tanvir', 60, 'B', 'Barisal'),
+(110, 'Fahim', 47, 'D', 'Sylhet');
+
+SELECT * FROM People;
+
+SET SQL_SAFE_UPDATES = 0;
+
+DELETE FROM People
+WHERE Marks < 50;
+
+SELECT * FROM People;
+
