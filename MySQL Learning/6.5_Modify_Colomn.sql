e85539eee71c061a0f4f24cdeb5d9049f1e4ad66
Rezwan <rezwanahmedtorabee@gmail.com>
2026-08-19 01:10:44 +0600

    SQL

---
+++ MySQL Learning/6.5_Modify_Colomn.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS Room;
+CREATE DATABASE Room;
+USE Room;
+
+CREATE TABLE People(
+    ID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Marks INT NOT NULL,
+    Grade VARCHAR(1),
+    City VARCHAR(50)
+);
+
+INSERT INTO People (ID, Name, Marks, Grade, City)
+VALUES
+(101, 'Alif', 30, 'F', 'Rangpur'),
+(102, 'Nice', 45, 'D', 'Dinajpur'),
+(103, 'Sahin', 85, 'C', 'Dhaka'),
+(104, 'Ripon', 68, 'B', 'Dinajpur'),
+(105, 'Prince', 89, 'A', 'Dhaka'),
+(106, 'Nayeem', 82, 'A', 'Chittagong'),
+(107, 'Rafi', 39, 'F', 'Khulna'),
+(108, 'Imran', 81, 'A', 'Rangpur'),
+(109, 'Tanvir', 60, 'B', 'Barisal'),
+(110, 'Fahim', 47, 'D', 'Sylhet');
+
+SELECT * FROM People;
+
+ALTER TABLE People
+MODIFY COLUMN Marks DECIMAL(5,2) NOT NULL;		-- Int to Decimal
+
+SELECT * FROM People;
